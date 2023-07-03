902fdc72949b273e452e8bb4c728e01b529d2f81
Anieb22 <anielloverdino@gmail.com>
2023-07-03 20:30:23 +0200

    eseguito snack 3

---
+++ Snack3/index.php
@@ -0,0 +1,51 @@
+<?php
+$postsByDate = [
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+// Stampare ogni data con i relativi post
+foreach ($postsByDate as $date => $posts) {
+    echo $date . ':<br>';
+    foreach ($posts as $post) {
+        echo '- Titolo: ' . $post['title'] . '<br>';
+        echo '  Autore: ' . $post['author'] . '<br>';
+        echo '  Testo: ' . $post['text'] . '<br>';
+    }
+    echo '<br>';
+}
+?>
